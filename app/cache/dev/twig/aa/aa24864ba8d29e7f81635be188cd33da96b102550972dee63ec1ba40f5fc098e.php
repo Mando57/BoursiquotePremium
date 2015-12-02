3eac97b2bda9758bf210722f7ff59440c58dc7f4
mando57 <themando57@gmail.com>
2015-12-02 15:59:16 +0100

    j

--- app/cache/dev/twig/aa/aa24864ba8d29e7f81635be188cd33da96b102550972dee63ec1ba40f5fc098e.php
+++ app/cache/dev/twig/aa/aa24864ba8d29e7f81635be188cd33da96b102550972dee63ec1ba40f5fc098e.php
@@ -22,49 +22,56 @@ class __TwigTemplate_a9bccf56eeebcd642ca8a23c427a709616bd30f026185d75f7cc18e83f1
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_c4db84d9372b118922bb02574c9700a36317841f6aa51dae3bf9a2613ae19f7e = $this->env->getExtension("native_profiler");
-        $__internal_c4db84d9372b118922bb02574c9700a36317841f6aa51dae3bf9a2613ae19f7e->enter($__internal_c4db84d9372b118922bb02574c9700a36317841f6aa51dae3bf9a2613ae19f7e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "BourseBundle:Profile:index.html.twig"));
+        $__internal_549c312dd35271c79055e7fbd4428705eb7ad8634d9a8708e73d61a45c219019 = $this->env->getExtension("native_profiler");
+        $__internal_549c312dd35271c79055e7fbd4428705eb7ad8634d9a8708e73d61a45c219019->enter($__internal_549c312dd35271c79055e7fbd4428705eb7ad8634d9a8708e73d61a45c219019_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "BourseBundle:Profile:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_c4db84d9372b118922bb02574c9700a36317841f6aa51dae3bf9a2613ae19f7e->leave($__internal_c4db84d9372b118922bb02574c9700a36317841f6aa51dae3bf9a2613ae19f7e_prof);
+        $__internal_549c312dd35271c79055e7fbd4428705eb7ad8634d9a8708e73d61a45c219019->leave($__internal_549c312dd35271c79055e7fbd4428705eb7ad8634d9a8708e73d61a45c219019_prof);
 
     }
 
     // line 3
     public function block_title($context, array $blocks = array())
     {
-        $__internal_49e88c4e8e205c9667dd035dcffe6431fd9a8db084f1ccbdfdca8201f57689ac = $this->env->getExtension("native_profiler");
-        $__internal_49e88c4e8e205c9667dd035dcffe6431fd9a8db084f1ccbdfdca8201f57689ac->enter($__internal_49e88c4e8e205c9667dd035dcffe6431fd9a8db084f1ccbdfdca8201f57689ac_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+        $__internal_c7194a458732cafe0cb864099cce85c61e01deca7a7f1a6377c214c1aff32741 = $this->env->getExtension("native_profiler");
+        $__internal_c7194a458732cafe0cb864099cce85c61e01deca7a7f1a6377c214c1aff32741->enter($__internal_c7194a458732cafe0cb864099cce85c61e01deca7a7f1a6377c214c1aff32741_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
 
         echo " ";
         $this->displayParentBlock("title", $context, $blocks);
         echo " - Accueil général ";
         
-        $__internal_49e88c4e8e205c9667dd035dcffe6431fd9a8db084f1ccbdfdca8201f57689ac->leave($__internal_49e88c4e8e205c9667dd035dcffe6431fd9a8db084f1ccbdfdca8201f57689ac_prof);
+        $__internal_c7194a458732cafe0cb864099cce85c61e01deca7a7f1a6377c214c1aff32741->leave($__internal_c7194a458732cafe0cb864099cce85c61e01deca7a7f1a6377c214c1aff32741_prof);
 
     }
 
     // line 5
     public function block_lateral($context, array $blocks = array())
     {
-        $__internal_177d1aabe0035fb0ed99442488a57c06ded24216caf36675f3f55ff1439dcc43 = $this->env->getExtension("native_profiler");
-        $__internal_177d1aabe0035fb0ed99442488a57c06ded24216caf36675f3f55ff1439dcc43->enter($__internal_177d1aabe0035fb0ed99442488a57c06ded24216caf36675f3f55ff1439dcc43_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "lateral"));
+        $__internal_adf9633e2bbb93cd0882cdf531f477cf896efec746e74e9304d034be35ffe03e = $this->env->getExtension("native_profiler");
+        $__internal_adf9633e2bbb93cd0882cdf531f477cf896efec746e74e9304d034be35ffe03e->enter($__internal_adf9633e2bbb93cd0882cdf531f477cf896efec746e74e9304d034be35ffe03e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "lateral"));
 
         // line 6
-        echo "    ";
+        echo "
+        <u>Mes valeurs favorites</u>
+    ";
+        // line 8
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["lesCategories"]) ? $context["lesCategories"] : null));
+        $context['_seq'] = twig_ensure_traversable((isset($context["lesBourses"]) ? $context["lesBourses"] : null));
         foreach ($context['_seq'] as $context["_key"] => $context["categ"]) {
-            // line 7
-            echo "        test
+            // line 9
+            echo "        ";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["categ"], "nom", array()), "html", null, true);
+            echo "
+
+
     ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['categ'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
         
-        $__internal_177d1aabe0035fb0ed99442488a57c06ded24216caf36675f3f55ff1439dcc43->leave($__internal_177d1aabe0035fb0ed99442488a57c06ded24216caf36675f3f55ff1439dcc43_prof);
+        $__internal_adf9633e2bbb93cd0882cdf531f477cf896efec746e74e9304d034be35ffe03e->leave($__internal_adf9633e2bbb93cd0882cdf531f477cf896efec746e74e9304d034be35ffe03e_prof);
 
     }
 
@@ -80,7 +87,7 @@ class __TwigTemplate_a9bccf56eeebcd642ca8a23c427a709616bd30f026185d75f7cc18e83f1
 
     public function getDebugInfo()
     {
-        return array (  60 => 7,  55 => 6,  49 => 5,  35 => 3,  11 => 1,);
+        return array (  63 => 9,  59 => 8,  55 => 6,  49 => 5,  35 => 3,  11 => 1,);
     }
 }
 /* {% extends "BourseBundle:layout:layout.html.twig" %}*/
@@ -88,7 +95,11 @@ class __TwigTemplate_a9bccf56eeebcd642ca8a23c427a709616bd30f026185d75f7cc18e83f1
 /* {% block title %} {{ parent() }} - Accueil général {% endblock %}*/
 /* */
 /* {% block lateral %}*/
-/*     {% for categ in lesCategories %}*/
-/*         test*/
+/* */
+/*         <u>Mes valeurs favorites</u>*/
+/*     {% for categ in lesBourses %}*/
+/*         {{ categ.nom }}*/
+/* */
+/* */
 /*     {% endfor %}*/
 /* {% endblock %}*/
