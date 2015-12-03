a2669e2d6a19fa9842183b2e6cab29ec31c3850b
mando57 <themando57@gmail.com>
2015-12-03 16:36:25 +0100

    test comit

--- web/images/graph/graph.php
+++ web/images/graph/graph.php
@@ -1,15 +1,46 @@
-<?php 
+<?php
+
+if(isset($_GET['t']))
+{
+    $title=$_GET['t'];
+}else{
+    $title='default title';
+}
+
+if(isset($_GET['id']))
+{
+    $ticker=$_GET['id'];
+
+}else{
+    //exit;
+}
+
+if(isset($_GET['tailleL']))
+{
+   $tailleL=$_GET['tailleL'];
+}else{
+    $tailleL=200;
+}
+
+if(isset($_GET['tailleH']))
+{
+    $tailleH=$_GET['tailleH'];
+
+}else{
+    $tailleH=200;
+}
+
 		include('PHPGraphLib.php');
         //$graph=models\PHPGraphLib::PHPGraphLib(650,200);
         //$graph = $this->container->get('la_fleur_symfony.panier');
-        $graph= new PHPGraphLib(200,200);
+        $graph= new PHPGraphLib($tailleL,$tailleH);
 
         $data = array("1" => .0032, "2" => .0028, "3" => .0021, "4" => .0033,
             "5" => .0034, "6" => .0031, "7" => .0036, "8" => .0027, "9" => .0024,
             "10" => .0021, "11" => .0026, "12" => .0024, "13" => .0036,
             "14" => .0028, "15" => .0025);
         $graph->addData($data);
-        $graph->setTitle('PPM Per Container');
+        $graph->setTitle($title);
         $graph->setBars(false);
         $graph->setLine(true);
         $graph->setDataPoints(true);
